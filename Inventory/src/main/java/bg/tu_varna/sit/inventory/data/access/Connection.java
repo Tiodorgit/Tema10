6fad36f87ab3052b42aaca07bb5f52f4b44c17bc
ProjectTUJava14 <92436546+ProjectTUJava14@users.noreply.github.com>
2022-02-06 10:17:22 +0200

    login screen

--- Inventory/src/main/java/bg/tu_varna/sit/inventory/data/access/Connection.java
+++ Inventory/src/main/java/bg/tu_varna/sit/inventory/data/access/Connection.java
@@ -5,12 +5,15 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+
 public class Connection {
     private static final Logger log = Logger.getLogger(Connection.class);
     private static SessionFactory sessionFactory;
     static {
         try{
-            sessionFactory = new Configuration().configure().buildSessionFactory();
+            Configuration configuration = new Configuration();
+            configuration = configuration.configure( "hibernate.cfg.xml" );
+            sessionFactory = configuration.buildSessionFactory();
         }catch (Throwable ex){
             log.error("Initial SessionFactory created failed " + ex);
         }
